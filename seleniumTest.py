ca4c7c4df4374924fad1ae4f04de28bb505c059a
Chulaka Ratnayake <choolake@gmail.com>
2018-02-01 15:57:06 +0530

    if clause to chek any error occured

--- seleniumTest.py
+++ seleniumTest.py
@@ -8,17 +8,22 @@ driver.get("https://eservices.railway.gov.lk/schedule")
 assert "Train" in driver.title
 selStart = Select(driver.find_element_by_xpath('.//*[@id=\'startStation\']'))
 startOptions = selStart.options
+print startOptions
 selEnd = Select(driver.find_element_by_xpath('.//*[@id=\'endStation\']'))
 endOptions = selEnd.options
-for startIndex in range(0, len(startOptions) - 1):
+for startIndex in range(1, len(startOptions) - 1):
     selStart.select_by_index(startIndex)
     # do stuff
-    for endIndex in range(0, len(endOptions) - 1):
+    for endIndex in range(1, len(endOptions) - 1):
         selEnd.select_by_index(endIndex)
         #click the button
         driver.find_element_by_xpath('.//*[@id=\'search_form_id\']/div/div[7]/div/button[1]').click()
         driver.implicitly_wait(10) # seconds
         te1 =driver.find_element_by_xpath(".//*[@id='es-content']/div[6]/div").text
-        print(te1)
-        driver.execute_script("window.history.go(-1)")
+        if not te1:
+             print( "error occured while start ".startIndex" and end ".endIndex)
+	else:
+            print(te1)
+            driver.execute_script("window.history.go(-1)")
 #driver.close()
+
